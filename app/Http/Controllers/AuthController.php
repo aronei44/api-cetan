e196e5be45e52016d419f648ef1a9fc49bb0a5c5
aronei44 <meliodasmeliodas224@gmail.com>
2022-02-07 18:57:46 +0700

    feat auth: login, register, logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required|min:8',
+        ]);
+        $user = User::where('email',$request->email)->first();
+        if(!$user){
+            return response()->json([
+                'message'=>'User not found',
+            ],404);
+        }else{
+            if(\Hash::check($request->password,$user->password)){
+                $token = $user->createToken('authToken')->plainTextToken;
+                $user->token = $token;
+                $user->status = 200;
+                return $user;
+            }else{
+                return response()->json([
+                    'message'=>'Password not match',
+                ],404);
+            }
+        }
+    }
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email:dns|unique:users',
+            'password'=>'required|min:8',
+        ]);
+        try {
+            $user = User::create([
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>\Hash::make($request->password),
+            ]);
+            $token = $user->createToken('authToken')->plainTextToken;
+            $user->token = $token;
+            $user->status = 201;
+            return $user;
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message'=>'something error',
+            ],500);
+        }
+    }
+    public function logout(Request $request){
+
+        try {
+            $tokens = \DB::select("DELETE FROM personal_access_tokens WHERE tokenable_id = " .$request->user()->id);
+            return response()->json([
+                'message'=>'Success Deleting Token'
+            ],200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message'=>'Unable to Deleting Token'
+            ],400);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/logout',[AuthController::class,'logout']);
 });
