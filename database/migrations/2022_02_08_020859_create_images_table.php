3790dcac139496d654e25f0edfd6248f3e51892b
aronei44 <meliodasmeliodas224@gmail.com>
2022-02-08 10:34:23 +0700

    fix migration

--- database/migrations/2022_02_08_020859_create_images_table.php
+++ database/migrations/2022_02_08_020859_create_images_table.php
@@ -15,7 +15,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('image_url');
+            $table->text('image_url');
             $table->boolean('primary')->default(true);
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
